3b83de184dbc3eb85a026b1badece648cca98840
kyberdrb <kyberdrb@gmail.com>
2018-08-02 20:31:26 +0200

    preparing for decorator

--- gui/tk_widget_operations.py
+++ gui/tk_widget_operations.py
@@ -1,26 +1,38 @@
+# TODO apply decorator to work on disabled text widgets as well
 def fill_text_box(pa_text_widget, pa_items):
-    activate_widgets_before_making_changes(pa_text_widget)
+    # TODO remove this line and add the abovementioned functionality via a decorator
+
+    was_active = was_widget_active(pa_text_widget)
+    if was_active != "normal":
+        activate_widgets_before_making_changes(pa_text_widget)
+
     for item in pa_items:
         item += "\n"
         pa_text_widget.insert("end", item)
+
+    if was_active != "normal":
+        deactivate_widgets(pa_text_widget)
+
 
 def clear_text_widgets(*pa_widgets):
     for widget in pa_widgets:
         clear_text_widget(widget)
 
 
-def clear_disabled_text_widgets(clear_text_widget_fun):
+def clear_disabled_text_widget(clear_text_widget_fun):
     def text_widget_wrapper(pa_text_widget):
-        if was_widget_active(pa_text_widget) != "normal":
+        was_active = was_widget_active(pa_text_widget)
+        if was_active  != "normal":
             activate_widgets_before_making_changes(pa_text_widget)
-            clear_text_widget_fun(pa_text_widget)
+
+        clear_text_widget_fun(pa_text_widget)
+
+        if was_active != "normal":
             deactivate_widgets(pa_text_widget)
-        else:
-            clear_text_widget_fun(pa_text_widget)
     return text_widget_wrapper
 
 
-@clear_disabled_text_widgets
+@clear_disabled_text_widget
 def clear_text_widget(pa_text_widget):
     pa_text_widget.delete("1.0", "end")
 
